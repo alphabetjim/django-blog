d0b17d279b0ee5c5ac6d76290e1b3c3c785dc700
James Bridgwater Court <james.bcourt@outlook.com>
2024-02-26 11:59:54 +0000

    Render collaboration request form

--- about/views.py
+++ about/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import About
+from .forms import CollaborateForm
 
 # Create your views here.
 def about_display(request):
@@ -8,8 +9,11 @@ def about_display(request):
     """
     about = About.objects.all().order_by("-updated_on").first()
 
+    collaborate_form = CollaborateForm()
+
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {"about": about,
+        "collaborate_form": collaborate_form,},
     )
